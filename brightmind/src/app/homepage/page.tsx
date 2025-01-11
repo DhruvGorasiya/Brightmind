111ce45aedb973b5ae5808721e2590aa08f5ab57
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2025-01-11 01:09:28 -0500

    homepage update

--- brightmind/src/app/homepage/page.tsx
+++ brightmind/src/app/homepage/page.tsx
@@ -43,11 +43,7 @@ export default function HomePage() {
             </div>
           </section>
 
-          <section
-            className={`grid ${
-              isSignedIn ? "md:grid-cols-2" : "md:grid-cols-1"
-            } gap-8`}
-          >
+          <section className="grid md:grid-cols-2 gap-8">
             <div
               className="bg-white/80 dark:bg-gray-800/90 rounded-xl p-6 shadow-lg hover:shadow-xl transition-all duration-300 backdrop-blur-sm border border-gray-100 dark:border-gray-700 cursor-pointer"
               onClick={() => router.push("/wellness-resource")}
@@ -61,7 +57,9 @@ export default function HomePage() {
               </p>
             </div>
 
-            <div className="bg-white/80 dark:bg-gray-800/90 rounded-xl p-6 shadow-lg hover:shadow-xl transition-all duration-300 backdrop-blur-sm border border-gray-100 dark:border-gray-700">
+            <div className="bg-white/80 dark:bg-gray-800/90 rounded-xl p-6 shadow-lg hover:shadow-xl transition-all duration-300 backdrop-blur-sm border border-gray-100 dark:border-gray-700"
+              onClick={() => router.push("/support-pathway")}
+            >
               <h3 className="text-2xl font-semibold mb-4 text-blue-600 dark:text-blue-400">
                 Support Pathways
               </h3>
